9ed9d137c71002f2282d7f16854efcd12961e100
surfingtux <hflautert@gmail.com>
2014-09-26 10:33:39 -0300

    Conf file
    Create conf file to get easy and clean settuping.

--- install.sh
+++ install.sh
@@ -4,9 +4,27 @@ BINARY=chkservices
 BINARY_TARGET=/usr/bin
 INITSCRIPT=init.d/chkservices
 INITSCRIPT_TARGET=/etc/init.d
+CONFIG=chkservices.conf
+CONFIG_TARGET=/etc/chkservices
 
 cp "$BINARY" "$BINARY_TARGET"
 cp "$INITSCRIPT" "$INITSCRIPT_TARGET"
+
+if [ -e $CONFIG_TARGET/$CONFIG ]
+then
+    echo
+    echo "-------------------------------------------------------------------"
+    echo "Existing configuration found. Creating $CONFIG_TARGET/$CONFIG.new."
+    echo "Update your existing configuration file with the new one or WFS may"
+    echo "not operate properly due to changes. Press enter to continue."
+    echo "-------------------------------------------------------------------"
+    read
+    cp "$CONFIG" "$CONFIG_TARGET/$CONFIG.new"
+    cp "$TARGETS" "$CONFIG_TARGET/$TARGETS.new"
+else
+    cp "$CONFIG" "$CONFIG_TARGET/$CONFIG"
+    cp "$TARGETS" "$CONFIG_TARGET/$TARGETS" 
+fi
 
 chmod 755 "$BINARY_TARGET/$BINARY"
 chmod 755 "$INITSCRIPT_TARGET/`basename $INITSCRIPT`"
